f73c228afa13c42bdf539fc2fc0c793a275ac7a5
DavidB <david.bunyamin.99@gmail.com>
2025-12-05 21:12:47 +0700

    fix status

--- src/routers/status.ts
+++ src/routers/status.ts
@@ -4,5 +4,5 @@ import { getSuccessCount } from "../services/mqtt_ser.js";
 export const statusRouter = Router();
 
 statusRouter.get("/", (_, res) => {
-    res.json({ success_count: getSuccessCount() });
+    res.json({ count: getSuccessCount() });
 });
